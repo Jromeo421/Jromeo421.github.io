f404c845e2a134da1756cd67488c69ece4b21ade
JJ Hugh <hug17010@byui.edu>
2018-07-28 18:29:06 -0500

    json data update

--- scripts/services.js
+++ scripts/services.js
@@ -14,24 +14,28 @@ serviceObject.onload = function() {
     document.getElementById("desc").innerHTML = serviceInfo.Services["0"].description
     document.getElementById("price").innerHTML = serviceInfo.Services["0"].price
     document.getElementById("other").innerHTML = serviceInfo.Services["0"].additional
+    
     //Second service
     document.getElementById("type1").innerHTML = serviceInfo.Services["1"].service
     document.getElementById("desc1").innerHTML = serviceInfo.Services["1"].description
     document.getElementById("price1").innerHTML = serviceInfo.Services["1"].price
-    document.getElementById("other").innerHTML = serviceInfo.Services["0"].additional
+    document.getElementById("other").innerHTML = serviceInfo.Services["1"].additional
+    
     //Third service
     document.getElementById("type2").innerHTML = serviceInfo.Services["2"].service
     document.getElementById("desc2").innerHTML = serviceInfo.Services["2"].description
     document.getElementById("price2").innerHTML = serviceInfo.Services["2"].price
-    document.getElementById("other").innerHTML = serviceInfo.Services["0"].additional
+    document.getElementById("other").innerHTML = serviceInfo.Services["2"].additional
+    
     //Fourth service
     document.getElementById("type3").innerHTML = serviceInfo.Services["3"].service
     document.getElementById("desc3").innerHTML = serviceInfo.Services["3"].description
     document.getElementById("price3").innerHTML = serviceInfo.Services["3"].price
-    document.getElementById("other").innerHTML = serviceInfo.Services["0"].additional
+    document.getElementById("other").innerHTML = serviceInfo.Services["3"].additional
+    
     //Fifth service
       document.getElementById("type4").innerHTML = serviceInfo.Services["4"].service
     document.getElementById("desc4").innerHTML = serviceInfo.Services["4"].description
     document.getElementById("price4").innerHTML = serviceInfo.Services["4"].price
-    document.getElementById("other").innerHTML = serviceInfo.Services["0"].additional
+    document.getElementById("other").innerHTML = serviceInfo.Services["4"].additional
 }
