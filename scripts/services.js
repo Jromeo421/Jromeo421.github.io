a2cbd0af0b7ed73279bf1d6ac904abe78143ecf5
JJ Hugh <hug17010@byui.edu>
2018-07-28 18:10:38 -0500

    Updates to services

--- scripts/services.js
+++ scripts/services.js
@@ -1,6 +1,6 @@
 // This is the Services Function
 var serviceObject = new XMLHttpRequest();
-serviceObject.open("GET", "https://sjkendall88.github.io/MountainSpoke/data/services.json");
+serviceObject.open("GET", "https://jromeo421.github.io/dataAccess/services.json");
 serviceObject.responseType = "json";
 serviceObject.send();
 /* -------------- Franklin, MN -------------- */
@@ -14,19 +14,19 @@ serviceObject.onload = function() {
     document.getElementById("desc").innerHTML = serviceInfo.Services["0"].description
     document.getElementById("price").innerHTML = serviceInfo.Services["0"].price
     //Second service
-    document.getElementById("type1").innerHTML = serviceInfo.Services["1"].name
+    document.getElementById("type1").innerHTML = serviceInfo.Services["1"].service
     document.getElementById("desc1").innerHTML = serviceInfo.Services["1"].description
     document.getElementById("price1").innerHTML = serviceInfo.Services["1"].price
     //Third service
-    document.getElementById("type2").innerHTML = serviceInfo.Services["2"].name
+    document.getElementById("type2").innerHTML = serviceInfo.Services["2"].service
     document.getElementById("desc2").innerHTML = serviceInfo.Services["2"].description
     document.getElementById("price2").innerHTML = serviceInfo.Services["2"].price
     //Fourth service
-    document.getElementById("type3").innerHTML = serviceInfo.Services["3"].name
+    document.getElementById("type3").innerHTML = serviceInfo.Services["3"].service
     document.getElementById("desc3").innerHTML = serviceInfo.Services["3"].description
     document.getElementById("price3").innerHTML = serviceInfo.Services["3"].price
     //Fifth service
-      document.getElementById("type4").innerHTML = serviceInfo.Services["4"].name
+      document.getElementById("type4").innerHTML = serviceInfo.Services["4"].service
     document.getElementById("desc4").innerHTML = serviceInfo.Services["4"].description
     document.getElementById("price4").innerHTML = serviceInfo.Services["4"].price
 }
