98f3f0435b028f011a1828e4673ef247de83e6de
JJ Hugh <hug17010@byui.edu>
2018-07-28 18:39:51 -0500

    updated services json

--- scripts/services.js
+++ scripts/services.js
@@ -19,25 +19,25 @@ serviceObject.onload = function() {
     document.getElementById("type1").innerHTML = serviceInfo.Services["1"].service
     document.getElementById("desc1").innerHTML = serviceInfo.Services["1"].description
     document.getElementById("price1").innerHTML = serviceInfo.Services["1"].price
-    document.getElementById("other").innerHTML = serviceInfo.Services["1"].additional
+    document.getElementById("other1").innerHTML = serviceInfo.Services["1"].additional
     
     //Third service
     document.getElementById("type2").innerHTML = serviceInfo.Services["2"].service
     document.getElementById("desc2").innerHTML = serviceInfo.Services["2"].description
     document.getElementById("price2").innerHTML = serviceInfo.Services["2"].price
-    document.getElementById("other").innerHTML = serviceInfo.Services["2"].additional
+    document.getElementById("other2").innerHTML = serviceInfo.Services["2"].additional
     
     //Fourth service
     document.getElementById("type3").innerHTML = serviceInfo.Services["3"].service
     document.getElementById("desc3").innerHTML = serviceInfo.Services["3"].description
     document.getElementById("price3").innerHTML = serviceInfo.Services["3"].price
-    document.getElementById("other").innerHTML = serviceInfo.Services["3"].additional
+    document.getElementById("other3").innerHTML = serviceInfo.Services["3"].additional
     
     //Fifth service
     document.getElementById("type4").innerHTML = serviceInfo.Services["4"].service
     document.getElementById("desc4").innerHTML = serviceInfo.Services["4"].description
     document.getElementById("price4").innerHTML = serviceInfo.Services["4"].price
-    document.getElementById("other").innerHTML = serviceInfo.Services["4"].additional
+    document.getElementById("other4").innerHTML = serviceInfo.Services["4"].additional
     
     //Sixth Service
     document.getElementById("type5").innerHTML = serviceInfo.Services["5"].service
